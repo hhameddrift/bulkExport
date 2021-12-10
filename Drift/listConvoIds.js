d007416c691827d47b504dbc8e1a27e163dfb8f2
Hazem Hamed <hhamed@drift.com>
2021-12-10 14:56:03 -0800

    testing 3 appended conversatiosn

--- Drift/listConvoIds.js
+++ Drift/listConvoIds.js
@@ -30,7 +30,7 @@ const data = JSON.stringify({
 
         //now-(1000*60*60*24*10),  //ensure this aligns with timer settings to run app.js
        // now
-        "2021-12-01",
+        "2021-11-01",
         "2021-12-10"
       ]
     }
